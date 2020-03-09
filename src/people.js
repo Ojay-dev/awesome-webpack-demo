e2c6df106f3d8350de3673677dbb8aa75baf75ae
Ojonugwa Justice Alikali <alikali.ojonugwajustice@gmail.com>
2020-03-09 06:49:51 +0100

    imitial commit

---
+++ src/people.js
@@ -0,0 +1,20 @@
+const people = [
+  {
+    manager: "Jen",
+    name: "Bob"
+  },
+  {
+    manager: "Jen",
+    name: "Sue"
+  },
+  {
+    manager: "Bob",
+    name: "Shirley"
+  },
+  {
+    manager: "Bob",
+    name: "Peter"
+  }
+]
+
+export default people
